0530d119120c95b72eafd94d9dc2199f2c6dab5d
Rionte <ramonsabro@gmail.com>
2021-01-02 21:03:14 -0500

    Create TeddySMPpatcher.bat

---
+++ TeddySMPpatcher.bat
@@ -0,0 +1,2 @@
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/Rionte/resource-packs/archive/master.zip', 'resource-packs-main.zip')"
+powershell -Command "Invoke-WebRequest https://github.com/Rionte/resource-packs/archive/master.zip -OutFile resource-packs-main.zip"
